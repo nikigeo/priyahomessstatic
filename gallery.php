a5b3a2274ba2b4441cb673fe1ec9c00576ce11dd
unknown <nikigeo22@gmail.com>
2020-01-05 22:09:55 +1100

    updates for priyahomes

--- gallery.php
+++ gallery.php
@@ -11,8 +11,8 @@
         <ul class="image-main-box clearfix">
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/1.jpg" alt="homestay at kochi" >
-                    <a href="images/gallery/1.jpg" class="more-details" data-title="Living Area">Living Area</a>
+                    <img src="images/gallery/thumbnails/1.jpg" alt="homestay at Koothattukulam" >
+                    <a href="images/gallery/1.jpg" class="more-details" data-title="Front Area">Front Area</a>
                     <figcaption>
                         <h4>Front <span>Area</span></h4>
                     </figcaption>
@@ -20,92 +20,92 @@
             </li>
             <li class="item col-xs-6 col-md-2" >
                 <figure>
-                    <img src="images/gallery/thumbnails/2.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/2.jpg" class="more-details" data-title="TV Hall Area">TV Hall Area</a>
+                    <img src="images/gallery/thumbnails/2.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/2.jpg" class="more-details" data-title="Front Area">Front Area</a>
                     <figcaption>
-                        <h4>TV <span>Hall Area</span></h4>
+                        <h4>Front <span>Area</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/3.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/3.jpg" class="more-details" data-title="Dinning Hall">Dinning Hall</a>
+                    <img src="images/gallery/thumbnails/3.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/3.jpg" class="more-details" data-title="Open Verandah">Open Verandah</a>
                     <figcaption>
-                        <h4>Dinning<span> Hall</span></h4>
+                        <h4>Open <span>Verandah</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/4.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/4.jpg" class="more-details" data-title="AC BedRoom">AC BedRoom</a>
+                    <img src="images/gallery/thumbnails/4.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/4.jpg" class="more-details" data-title="SideView">SideView</a>
                     <figcaption>
-                        <h4>AC<span> BedRoom</span></h4>
+                        <h4>Side<span> View</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/5.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/5.jpg" class="more-details" data-title="BedRoom">BedRoom</a>
+                    <img src="images/gallery/thumbnails/5.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/5.jpg" class="more-details" data-title="Living Room">Living Room</a>
                     <figcaption>
-                        <h4>BedRoom</h4>
+                        <h4>Living <span>Room</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/6.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/6.jpg" class="more-details" data-title="BedRoom">BedRoom</a>
+                    <img src="images/gallery/thumbnails/6.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/6.jpg" class="more-details" data-title="Living Room">Living Room</a>
                     <figcaption>
-                        <h4>BedRoom</h4>
+                        <h4>Living <span>Room</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/7.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/7.jpg" class="more-details" data-title="Kitchen">Kitchen</a>
+                    <img src="images/gallery/thumbnails/7.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/7.jpg" class="more-details" data-title="Dinning">Dinning</a>
                     <figcaption>
-                        <h4>Kitchen</h4>
+                        <h4>Dinning</h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/8.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/8.jpg" class="more-details" data-title="Balcony Area">Balcony Area</a>
+                    <img src="images/gallery/thumbnails/8.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/8.jpg" class="more-details" data-title="Dinning">Dinning</a>
                     <figcaption>
-                        <h4>Balcony<span> Area</span></h4>
+                        <h4>Dinning</span></h4>
                     </figcaption>
                 </figure>
             </li>
 
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/9.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/9.jpg" class="more-details" data-title="Garden Area">Garden Area</a>
+                    <img src="images/gallery/thumbnails/9.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/9.jpg" class="more-details" data-title="Double Bedroom">Double Bedroom</a>
                     <figcaption>
-                        <h4>Garden<span> Area</span></h4>
+                        <h4>Single <span>Bedroom</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/10.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/10.jpg" class="more-details" data-title="Front Entrance">Front Entrance</a>
+                    <img src="images/gallery/thumbnails/10.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/10.jpg" class="more-details" data-title="Single Bedroom">Single Bedroom</a>
                     <figcaption>
-                        <h4>Front<span> Entrance</span></h4>
+                        <h4>Single<span> Bedroom</span></h4>
                     </figcaption>
                 </figure>
             </li>
             <li class="item col-xs-6 col-md-2">
                 <figure>
-                    <img src="images/gallery/thumbnails/11.jpg" alt="homestay at Kochi"/>
-                    <a href="images/gallery/11.jpg" class="more-details" data-title="Neighbouring Area-2">Our Neighbourhood</a>
+                    <img src="images/gallery/thumbnails/11.jpg" alt="homestay at Koothattukulam"/>
+                    <a href="images/gallery/11.jpg" class="more-details" data-title="Kitchen">Kitchen</a>
                     <figcaption>
-                        <h4><span>Our</span> Neighborhood</h4>
+                        <h4>Kitchen</h4>
                     </figcaption>
                 </figure>
             </li>            
