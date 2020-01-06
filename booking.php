65b6ab21a30bf3288033b245cd324816e1d25eec
unknown <nikigeo22@gmail.com>
2020-01-06 21:02:34 +1100

    updates for priyahomes

--- booking.php
+++ booking.php
@@ -3,6 +3,14 @@
 $_REQUEST["pagename"] = "booking";
 ?>
 <body class="homepage trans-header sticky white-datepicker">
+<?php include ('topbar.php'); ?>
+<div class="internal-page-title about-page">
+    <h1>Booking</span></h1>
+    <ol class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li class="active">Booking</li>
+    </ol>
+</div>
 
 <?php include ('homestaymessage.php'); ?>
 <?php include ('footer.php'); ?>
