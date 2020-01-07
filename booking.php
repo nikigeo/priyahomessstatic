9f7e8d69b1e0b18a1d304bbe86d4ad0199395275
unknown <nikigeo22@gmail.com>
2020-01-07 22:04:57 +1100

    updates for priyahomes

--- booking.php
+++ booking.php
@@ -5,11 +5,24 @@ $_REQUEST["pagename"] = "booking";
 <body class="homepage trans-header sticky white-datepicker">
 <?php include ('topbar.php'); ?>
 <div class="internal-page-title about-page">
-    <h1>Booking</span></h1>
+    <h1>Booking Online</span></h1>
     <ol class="breadcrumb">
         <li><a href="index.php">Home</a></li>
         <li class="active">Booking</li>
     </ol>
+</div>
+<div id="welcome" class="container">
+    <!-- Inner section -->
+    <div class="inner-content">
+		<h3> 
+			<a href="https://www.instamojo.com/@priyahomes/">
+				<img src="images/pay-online-now.jpg" "alt="pay online">
+			</a>
+			<a href="https://www.payumoney.com/customer/users/paymentOptions/#/E08FC403FFFB7E1F8EF90EC7BA23EE78/priyahomes/187823 ">
+				<img src="images/pay-online.png" "alt="pay online">
+			</a>
+        </h3>
+    </div>
 </div>
 
 <?php include ('homestaymessage.php'); ?>
