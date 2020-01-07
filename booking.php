f29c6ae72d2734ce47db450610009f0378ddf0be
unknown <nikigeo22@gmail.com>
2020-01-07 23:03:59 +1100

    updates for priyahomes

--- booking.php
+++ booking.php
@@ -13,15 +13,13 @@ $_REQUEST["pagename"] = "booking";
 </div>
 <div id="welcome" class="container">
     <!-- Inner section -->
-    <div class="inner-content">
-		<h3> 
-			<a href="https://www.instamojo.com/@priyahomes/">
-				Pay Now With instamojo<img src="images/instamojo.png" "alt="pay online">
-			</a><br /><br/>
-			<a href="https://www.payumoney.com/customer/users/paymentOptions/#/E08FC403FFFB7E1F8EF90EC7BA23EE78/priyahomes/187823 ">
-				Pay Now With PayU<img src="images/payu.png" "alt="pay online">
-			</a>
-        </h3>
+    <div class="inner-content">	
+		<a href="https://www.instamojo.com/@priyahomes/">
+			Pay Now With instamojo<img src="images/instamojo.png" "alt="pay online">
+		</a><br/>
+		<a href="https://www.payumoney.com/customer/users/paymentOptions/#/E08FC403FFFB7E1F8EF90EC7BA23EE78/priyahomes/187823 ">
+			Pay Now With PayU<img src="images/payu.png" "alt="pay online">
+		</a>
     </div>
 </div>
 
