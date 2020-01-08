74856aa70de5d2b1d7053a70e971f7834aa10970
unknown <nikigeo22@gmail.com>
2020-01-08 20:28:42 +1100

    updates for priyahomes

--- facilities.php
+++ facilities.php
@@ -4,7 +4,7 @@ $_REQUEST["pagename"] = "facilities";
 ?>
 <body class="homepage trans-header sticky white-datepicker">
 <?php include ('topbar.php'); ?>
-<div class="internal-page-title about-page" data-parallax="scroll" data-image-src="images/internal-header.jpg">
+<div class="internal-page-title about-page>
     <h1>Facilities in<span> Priya Homes</span></h1>
     <ol class="breadcrumb"><!-- Internal Page Breadcrumb -->
         <li><a href="index.php">Home</a></li>
--- gallerydetails.php
+++ gallerydetails.php
@@ -4,7 +4,7 @@ $_REQUEST["pagename"] = "gallery";
 ?>
 <body class="homepage trans-header sticky white-datepicker">
 <?php include ('topbar.php'); ?>
-<div class="internal-page-title about-page" data-parallax="scroll" data-image-src="images/internal-header.jpg">
+<div class="internal-page-title about-page">
     <h1> <span>Priya Homes</span> Gallery</h1>
     <ol class="breadcrumb"><!-- Internal Page Breadcrumb -->
         <li><a href="index.php">Home</a></li>
