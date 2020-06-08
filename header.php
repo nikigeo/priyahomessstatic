139ef40a8cdc520434f650a3fd010675a8050229
nikigeo <nikigeo22@gmail.com>
2020-06-08 23:09:28 +1000

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <link rel="icon" type="image/png" href="images/logo/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Priya Homes is an affordable home stay at Koothattukulam, Ernakulam, Provides accomdation for working women">
-<meta name="keywords" content="homestay Koothattukulam,home stay koothattukulam, home stay Ernakulam, women's hostel Koothattukulam, working women's hostel Koothattukulam Ernakulam, Furnished Apartment for monthly and daily rent"  />
+<meta name="keywords" content="homestay Koothattukulam,home stay koothattukulam, home stay Ernakulam, women's hostel Koothattukulam, working women's hostel Koothattukulam Ernakulam, Furnished Apartment for monthly and daily rent, Paying Guest House, Women Paying Guest House, Working Women Paying Guest House, Working Women Guest House"  />
 <meta name="robots" content="index,follow"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='https://fonts.googleapis.com/css?family=Lobster%7cRaleway:400,300,100,600,700,800' rel='stylesheet' type='text/css'>
