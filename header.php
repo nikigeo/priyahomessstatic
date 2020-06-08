288761c1d7a6ec8817d99ca25c1c1b3eff1ce3a7
nikigeo <nikigeo22@gmail.com>
2020-06-08 23:04:34 +1000

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<title>Priya Homes at Koothattukulam Kerala</title>
 <link rel="icon" type="image/png" href="images/logo/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Athithi Homestay at Kochi is a good and better place to staying with different experience and affordable for visiting kadavanthra and vyttila at Ernakulam City">
+<meta name="description" content="Priya Homes is an affordable home stay at Koothattukulam, Ernakulam, Provides accomdation for working women">
+<meta name="keywords" content="homestay Koothattukulam,home stay koothattukulam, home stay Ernakulam, women's hostel Koothattukulam, working women's hostel Koothattukulam Ernakulam, Furnished Apartment for monthly and daily rent"  />
+<meta name="robots" content="index,follow"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='https://fonts.googleapis.com/css?family=Lobster%7cRaleway:400,300,100,600,700,800' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
